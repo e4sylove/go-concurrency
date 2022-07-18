0a7897ba9f990942fcb75eb01ccb484578118e82
Hieu Nguyen <hieungming@gmail.com>
2022-07-18 23:14:26 +0700

    crawl 10000 url example

---
+++ crawl/crawl.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	urlCh := make(chan int, 10)
+
+	for i := 0; i < 5; i++ {
+		go crawl(i, urlCh)
+	}
+
+	for j := 0; j < 10000; j++ {
+		urlCh <- j
+	}
+}
+
+func crawl(w int, ch chan int) {
+
+	for i := range ch {
+		time.Sleep(time.Second * 1)
+		fmt.Printf("Woker %v crawled url %v\n", w, i)
+	}
+}
+
+
